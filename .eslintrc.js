c640d1283f9a9d39c38a4eb2a443cf358730c99d
Rodrigo <rodrigo.coffani@gmail.com>
2023-04-26 17:45:23 -0300

    ✅ Add ESLint and update manifest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,31 @@
 export default {
   'env': {
-  'browser': true,
-		'es2021': true
-	},
-	'extends': 'eslint:recommended',
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			2
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
+    'browser': true,
+      'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
         
-	}
+  }
 }
